447691e806fce4bfb8ba5267e9337a6996a7c939
Martens <koen.martens@wur.nl>
2018-03-27 11:54:22 +0200

    Trying preloading of images

---
+++ javascript/preload_figures.js
@@ -0,0 +1,8 @@
+var images = [];
+function loadExplodedImages() {
+    for (var i = 0; i < 21; i++) {
+        images[i] = new Image();
+		var imsrc = './images/miCube_exploded_animation/smallSize/exploded_${i}.png';
+		images[i].src = imsrc;
+    }
+}
