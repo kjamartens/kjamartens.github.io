f80815eb697a3834b91849cc5b4711d5c9bcdf5e
Martens <koen.martens@wur.nl>
2018-03-27 11:41:49 +0200

    Changed exploded view added rotating view
    Signed-off-by: Martens <koen.martens@wur.nl>

--- javascript/update_figures.js
+++ javascript/update_figures.js
@@ -1,19 +1,28 @@
-// Figure 2 interactively loads static images, stored locally
-function update_figure_2() {
-  var period = document.getElementById("Figure_2_period").value;
-  var filename = "./images/figure_2/period_" + period + ".svg";
-  var image = document.getElementById("Figure_2_image");
+
+// Figure Exploded interactively loads static images, stored locally
+function update_figure_exploded() {
+  var period = document.getElementById("Figure_exploded_period_slider").value;
+  var filename = "./images/miCube_exploded_animation/smallSize/exploded_" + period + ".png";
+  var image = document.getElementById("Figure_exploded_image");
+  image.src = filename;
+}
+// Figure Rotated interactively loads static images, stored locally
+function update_figure_rotated() {
+  var period = document.getElementById("Figure_rotated_period_slider").value;
+  var id = 0;
+  if (period > 0){
+	  id = period % 50;
+  }else if (period < 0){
+	  id = 50-Math.abs(period);
+  }else{
+	  id = 0;
+  }
+  var filename = "./images/miCube_rotated_animation/smallSize/rotated_" + id + ".png";
+  var image = document.getElementById("Figure_rotated_image");
   image.src = filename;
 }
 
 
-// Figure 2b interactively loads static images, stored locally
-function update_figure_2b() {
-  var period = document.getElementById("Figure_2b_period_slider").value;
-  var filename = "./images/miCube_exploded_animation/miCube-" + period + ".bmp";
-  var image = document.getElementById("Figure_2b_image");
-  image.src = filename;
-}
 
 // Figure 3 interactively loads static images.
 // Check if our big images are available locally or remotely:
